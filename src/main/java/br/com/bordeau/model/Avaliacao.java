ffa964258a978b059c31756a46704109a862c009
Rodrigo Tomazi <rmtomazi@gmail.com>
2018-09-03 11:35:41 -0300

    Adicionadas as classes Avaliacao, Usuario e Endereco

--- src/main/java/br/com/bordeau/model/Avaliacao.java
+++ src/main/java/br/com/bordeau/model/Avaliacao.java
@@ -1,5 +1,27 @@
 package br.com.bordeau.model;
 
-public class Avaliacao {
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
+public class Avaliacao {
+	@Id
+	private Long id;
+	private Double avaliacao;
+	private Usuario usuario;
+	
+	//Getters e Setters
+	public Double getAvaliacao() {
+		return avaliacao;
+	}
+	public void setAvaliacao(Double avaliacao) {
+		this.avaliacao = avaliacao;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
 }
---
+++ src/main/java/br/com/bordeau/model/Endereco.java
@@ -0,0 +1,5 @@
+package br.com.bordeau.model;
+
+public class Endereco {
+
+}
