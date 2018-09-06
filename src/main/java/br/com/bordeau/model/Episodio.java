be67bef9910919daa925363d65fcc47fbfa99cdb
rmtomazi <rmtomazi@gmail.com>
2018-09-06 08:59:28 -0300

    Adicionada as classes Categoria, Episodio, Playlist e TipoUsuario e atualizada as classes Avaliacao, Endereco, Podcast e Usuario

---
+++ src/main/java/br/com/bordeau/model/Episodio.java
@@ -0,0 +1,81 @@
+package br.com.bordeau.model;
+
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Episodio {
+	@Id
+	private Long id;
+	private String titulo;
+	private String audio;
+	private String descricao;
+	private String capa;
+	private String htmlComplementar;
+	@OneToMany
+	private List<Comentario> comentarios;
+	@OneToMany
+	private List<Avaliacao> avaliacoes;
+	@ManyToOne
+	private Playlist playlist;
+	
+	//Getters e Setters
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAudio() {
+		return audio;
+	}
+	public void setAudio(String audio) {
+		this.audio = audio;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getCapa() {
+		return capa;
+	}
+	public void setCapa(String capa) {
+		this.capa = capa;
+	}
+	public String getHtmlComplementar() {
+		return htmlComplementar;
+	}
+	public void setHtmlComplementar(String htmlComplementar) {
+		this.htmlComplementar = htmlComplementar;
+	}
+	public List<Comentario> getComentarios() {
+		return comentarios;
+	}
+	public void setComentarios(List<Comentario> comentarios) {
+		this.comentarios = comentarios;
+	}
+	public List<Avaliacao> getAvaliacoes() {
+		return avaliacoes;
+	}
+	public void setAvaliacoes(List<Avaliacao> avaliacoes) {
+		this.avaliacoes = avaliacoes;
+	}
+	public Playlist getPlaylist() {
+		return playlist;
+	}
+	public void setPlaylist(Playlist playlist) {
+		this.playlist = playlist;
+	}
+	
+}
