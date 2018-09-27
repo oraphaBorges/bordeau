794d3b170e3b37216d578f0862c7bb470fcfa366
Raphael Borges <rah-borges@hotmailcom>
2018-09-27 09:21:51 -0300

    Criado a validação dos campos podcast

---
+++ src/main/java/br/com/bordeau/validations/PodcastValidation.java
@@ -0,0 +1,23 @@
+package br.com.bordeau.validations;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import br.com.bordeau.model.Podcast;
+
+public class PodcastValidation implements Validator{
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return Podcast.class.isAssignableFrom(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		String required = "field.required";
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors,"titulo", required);
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors,"descricao", required);
+	}
+
+}
